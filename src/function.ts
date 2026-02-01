38edec3b2742dbd68619853c68a42be4a99d90d9
alaminislamrahat <alaminislamrahat@gmail.com>
2026-02-01 14:18:33 +0600

    method call back in typescript

--- src/function.ts
+++ src/function.ts
@@ -8,3 +8,21 @@ const addArrow = (num1: number, num2: number) : number => num1 + num2;
 addNomrmal(2, 2)
 
 addArrow(2, 2)
+
+
+// obj => function => method
+
+const poorUser = {
+    name : "Rahat",
+    balance: 0,
+    addBallance(value: number): number{
+        return this.balance + value;
+    }
+}
+
+
+
+//call back 
+
+const arr: number[] = [1, 2, 3];
+const sqrArray = arr.map((element: number) : number => element * element)
